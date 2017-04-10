9391166904d06d1ef5ca59053f43dc2298443a30
Haris Iqbal <harrisiqba@gmail.com>
2017-04-09 20:54:38 -0400

    first commit

---
+++ js/new-age.js
@@ -0,0 +1,45 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    })
+
+    var $jittery = $('.jittery span'),
+      aText    = $jittery.text().split(''),
+      letters = '';
+  
+    for(var i = 0; i < aText.length; i++){
+        letters += '<span>'+aText[i]+'</span>';
+    }
+    
+    $jittery.empty().append(letters);
+    
+    $.each($('span', $jittery), function(i){
+        $(this).css('animation-delay', '-'+i+'70ms');
+    });
+
+})(jQuery); // End of use strict
